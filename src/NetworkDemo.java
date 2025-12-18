11b01ce246b1afd000c150ec46ce625d8dfa0d36
DaniiBaenaa <daniel.baena@enti.cat>
2025-12-18 16:21:13 +0100

    asds

--- src/NetworkDemo.java
+++ src/NetworkDemo.java
@@ -29,6 +29,5 @@ public class NetworkDemo {
         System.out.println("Buscar IP 172.16.0.20: " + red.buscarPorIP("172.16.0.20"));
         System.out.println("Vulnerables alcanzables desde atacante: " + red.contarVulnerablesAlcanzables(atacante));
         System.out.println("Nodos aislados: " + red.nodosAislados());
-        System.out.println("jesuisus");
     }
 }
